b8f4466863f803826cdf5815d263f0cbcc55171f
chirag-mandaviya-itechcloud <chirag.mandaviya@itechcloudsolution.com>
2025-12-30 16:56:41 +0530

    Add Pdf vf page and lwc code

--- force-app/main/default/lwc/pdfGenerator/pdfGenerator.js
+++ force-app/main/default/lwc/pdfGenerator/pdfGenerator.js
@@ -1,4 +1,61 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
+import getInvoiceDetails from '@salesforce/apex/PdfGeneratorController.getInvoiceDetails';
 export default class PdfGenerator extends LightningElement {
+    recordId;
+    invoiceDetails = {};
+    templateName = '';
+    isLoading = true;
 
+    @wire(CurrentPageReference)
+    getPageRef(pageRef) {
+        if (pageRef) {
+            // Quick Action recordId comes from state.recordId
+            this.recordId = pageRef.state?.recordId;
+
+            if (this.recordId) {
+                console.log('Record ID from PageReference:', this.recordId);
+                this.fetchDefaultInvoiceLayout();
+            }
+        }
+    }
+
+    fetchDefaultInvoiceLayout() {
+        this.isLoading = true;
+        console.log('Record ID:', this.recordId);
+        getInvoiceDetails({ invoiceId: this.recordId })
+            .then(result => {
+                console.log('Invoice Details:', result);
+                this.invoiceDetails = result;
+                this.setTemplateName();
+                this.isLoading = false;
+            })
+            .catch(error => {
+                console.error('Error fetching invoice details:', error);
+                this.isLoading = false;
+            });
+    }
+
+    setTemplateName() {
+        // Set the template name based on your logic
+        this.templateName = 'Default_Layout';
+
+        if (this.invoiceDetails?.s2p3__To_Account__r?.s2p3__Default_Invoice_Layout__c) {
+            this.templateName = this.invoiceDetails.s2p3__To_Account__r.s2p3__Default_Invoice_Layout__c;
+        }
+        console.log('Template Name set to:', this.templateName);
+    }
+
+    get iframeUrl() {
+        if (!this.invoiceDetails?.Id || !this.templateName) {
+            return '';
+        }
+
+        return `/apex/InvoicePDF?templateDevName=${this.templateName}&whatId=${this.invoiceDetails.Id}`;
+    }
+
+    get showIframe() {
+        console.log('iframeUrl:', this.iframeUrl);
+        return !this.isLoading && this.iframeUrl;
+    }
 }
