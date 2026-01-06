a0f1d69da06c5bd3507de3a69107e9e4fd1c058d
chirag-mandaviya-itechcloud <chirag.mandaviya@itechcloudsolution.com>
2026-01-06 10:14:29 +0530

    Controller Added and changes in lwc

--- force-app/main/default/lwc/sendInvoiceEmail/sendInvoiceEmail.js
+++ force-app/main/default/lwc/sendInvoiceEmail/sendInvoiceEmail.js
@@ -1,7 +1,17 @@
 import { LightningElement, api } from 'lwc';
+import LightningAlert from 'lightning/alert';
 export default class SendInvoiceEmail extends LightningElement {
     @api recordIds;
     connectedCallback() {
         console.log("record ids: ", this.recordIds);
+        this.showAlert('Success', 'Email sending process initiated', 'success');
+    }
+
+    async showAlert(title, message, theme = 'info') {
+        await LightningAlert.open({
+            message,
+            theme,
+            label: title
+        });
     }
 }
