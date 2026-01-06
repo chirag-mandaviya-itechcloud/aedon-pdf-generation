0b90f06bf46b3c3a1712b8cee798c1cc7d174e46
chirag-mandaviya-itechcloud <chirag.mandaviya@itechcloudsolution.com>
2026-01-06 11:59:49 +0530

    changes  in send invoice email batch and controller

--- force-app/main/default/lwc/sendInvoiceEmail/sendInvoiceEmail.js
+++ force-app/main/default/lwc/sendInvoiceEmail/sendInvoiceEmail.js
@@ -5,10 +5,11 @@ export default class SendInvoiceEmail extends LightningElement {
     @api recordIds;
     connectedCallback() {
         console.log("record ids: ", this.recordIds);
+        this.checkRecordsIsSelected();
         if (this.recordIds) {
             sendInvoiceEmailFunctionality({ invoiceIds: this.recordIds })
                 .then(result => {
-                    this.showAlert('Success', result, 'success');
+                    this.showAlertAndReturn('Success', result, 'success');
                 })
                 .catch(error => {
                     console.error('Error sendInvoiceEmailFunctionality : ', error);
@@ -17,11 +18,25 @@ export default class SendInvoiceEmail extends LightningElement {
 
     }
 
-    async showAlert(title, message, theme = 'info') {
+    checkRecordsIsSelected() {
+        if (!this.recordIds) {
+            this.showAlertAndReturn('Error', 'Please Select Invoices', 'error');
+        }
+    }
+
+    redirectToListView() {
+        url = `/lightning/o/s2p3__Sales_Invoice__c/list?filterName=s2p3__All`
+        window.location.replace(url);
+    }
+
+    async showAlertAndReturn(title, message, theme = 'success') {
         await LightningAlert.open({
             message,
             theme,
             label: title
         });
+        setTimeout(() => {
+            this.redirectToListView();
+        }, 300);
     }
 }
